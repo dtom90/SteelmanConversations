8c33453edf4dd2fc4c0f029333c3b52389754c1c
David Thomason <911671+dtom90@users.noreply.github.com>
2025-06-21 10:49:44 -0500

    adds .steel-text

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center mt-20 text-center max-w-screen-lg mx-auto">
-      <h1 className="text-3xl font-bold bg-gradient-to-br from-gray-400 via-gray-50 to-gray-400 bg-clip-text text-transparent drop-shadow-lg">
+    <div className="flex flex-col items-center justify-center text-center max-w-screen-lg mx-auto">
+
+      <h1 className="mt-20 steel-text">
         <div className="text-7xl">Steel Man</div>
         <div className="text-5xl">Conversations</div>
       </h1>
+
       <img
         src="/SteelmanConversations/Tucker-Crux-Lex.png"
         alt="Tucker Carlson, Lex Fridman, and Ted Cruz"
-        className="mt-8"
+        className="mt-10"
       />
+
+      <div className="my-20 steel-text">
+        <p>
+          Bringing higher quality conversations to the world
+        </p> 
+      </div>
     </div>
   )
 }
