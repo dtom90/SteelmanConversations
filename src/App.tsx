5223377cc468fd595fff02e8665ed6ceca6f657b
David Thomason <911671+dtom90@users.noreply.github.com>
2025-06-20 22:43:08 -0500

    adds image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center mt-20 text-center">
+    <div className="flex flex-col items-center justify-center mt-20 text-center max-w-screen-lg mx-auto">
       <h1 className="text-3xl font-bold bg-gradient-to-br from-gray-400 via-gray-50 to-gray-400 bg-clip-text text-transparent drop-shadow-lg">
         <div className="text-7xl">Steel Man</div>
         <div className="text-5xl">Conversations</div>
       </h1>
+      <img
+        src="/SteelmanConversations/Tucker-Crux-Lex.png"
+        alt="Tucker Carlson, Lex Fridman, and Ted Cruz"
+        className="mt-8"
+      />
     </div>
   )
 }
